51833859d8cdcc80a320a13e1d0c91ad330f8fe9
zenLabX <simple.forwork2025@gmail.com>
2025-09-13 16:04:21 +0800

    fix: 添加 Suspense，為了等 Content 裡的非同步操作完成再渲染

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Layout from "@/components/layout";
 import Content from "@/modules/home/content";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <Layout>
-      <Content />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Content />
+      </Suspense>
     </Layout>
   );
 }
